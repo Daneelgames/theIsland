e82c9cf19819f5d31a74ead289856e909dfa41c3
da44neel <rubberandrobber@gmail.com>
2021-09-28 12:14:42 +0300

    room navigation is generating

--- Assets/NavigationRoom.cs
+++ Assets/NavigationRoom.cs
@@ -19,95 +19,10 @@ public class NavigationRoom : MonoBehaviour
     public Transform tilesParent;
     [SerializeField] NavigationManager _navigationManager;
 
-    [ContextMenu("GenerateNavigationTiles")]
-    public void GenerateNavigationTiles()
-    {
-        // GET BOUNDS
-        width = Mathf.Abs(bounds.leftBound.transform.localPosition.x) + Mathf.Abs(bounds.rightBound.transform.localPosition.x);
-        width /= _navigationManager.tileSize;
-        height = Mathf.Abs(bounds.topBound.transform.localPosition.y) + Mathf.Abs(bounds.bottomBound.transform.localPosition.y);
-        height /= _navigationManager.tileSize;
-        length = Mathf.Abs(bounds.forwardBound.transform.localPosition.z) + Mathf.Abs(bounds.backBound.transform.localPosition.z);
-        length /= _navigationManager.tileSize;
-
-        roomSize = new Vector3Int(Mathf.RoundToInt(width ), Mathf.RoundToInt(height), Mathf.RoundToInt(length));
-        
-        tiles = new Tile[roomSize.x, roomSize.y,roomSize.z];
-        Debug.Log(tiles.Length);
-
-        /*
-        var tempPos = transform.position + new Vector3(bounds.leftBound.localPosition.x, bounds.bottomBound.localPosition.y, bounds.forwardBound.position.z);
-        GameObject zeroObject = new GameObject("Tile 0-0-0");
-        zeroObject.transform.position = tempPos;
-        zeroObject.transform.parent = tilesParent;
-        var collider = zeroObject.AddComponent<BoxCollider>();
-        collider.size = new Vector3(4.5f,4.5f,4.5f);
-        */
-            
-        // 0,0,0 is front left bottom tile
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                for (int z = 0; z < length; z++)
-                {
-                    tiles[x, y, z] = new Tile();
-
-                    tiles[x, y, z].worldPosition = transform.position + 
-                            new Vector3(bounds.leftBound.localPosition.x, bounds.topBound.localPosition.y, bounds.forwardBound.position.z) + 
-                        new Vector3( _navigationManager.tileSize * x, _navigationManager.tileSize * y,_navigationManager.tileSize * z);
-                    
-                    Debug.Log(tiles[x, y, z].worldPosition);
-                    var tempPos = tiles[x, y, z].worldPosition;
-                    GameObject tileObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    tileObject.name = "Tile " + x + "; "+ y + "; " + z + ";";
-                    tileObject.transform.position = tempPos;
-                    tileObject.transform.localScale = new Vector3(4.5f,4.5f,4.5f);
-                    tileObject.transform.parent = tilesParent;
-                    var meshRenderer = tileObject.GetComponent<MeshRenderer>();
-                    meshRenderer.material = _navigationManager.freeTileMaterial;
-
-                    tiles[x, y, z].tileObject = tileObject;
-                    tilesSpawned.Add(tiles[x, y, z]);
-                    /*
-                    var collider = zeroObject.AddComponent<BoxCollider>();
-                    collider.size = new Vector3(4.5f,4.5f,4.5f);
-                    */
-                }
-            }
-        }
-    }
-
-    [ContextMenu("ClearTiles")]
-    public void ClearTiles()
-    {
-        if (tiles == null || tiles.Length <= 0)
-            return;
-        
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                for (int z = 0; z < length; z++)
-                {
-                    if (tiles[x, y, z] != null && tiles[x,z,y].tileObject != null)
-                    {
-                        tiles[x, y, z] = null;
-                        Destroy(tiles[x,z,y].tileObject);
-                    }
-                }
-            }
-        }
-
-        tiles = null;
-    }
+    public LayerMask obstaclesLayerMask;
 
     IEnumerator Start()
     {
-        ClearTiles();
-        GenerateNavigationTiles();
-        //CheckNavigationTilesForObstacles();
-        
         while (true)
         {
             if (tilesSpawned.Count <= 0)
@@ -129,12 +44,127 @@ public class NavigationRoom : MonoBehaviour
             }
         }
     }
+
+    [ContextMenu("GenerateRoomNavigation")]
+    public void GenerateRoomNavigation()
+    {
+        ClearTiles();
+        InitTiles();
+        GenerateNavigationTilesDebugVisuals();
+        BoxcastAgainstSpawnedDebugTiles();
+    }
+
+    void InitTiles()
+    {
+        // GET BOUNDS
+        width = Mathf.Abs(bounds.leftBound.transform.localPosition.x) + Mathf.Abs(bounds.rightBound.transform.localPosition.x);
+        width /= _navigationManager.tileSize;
+        height = Mathf.Abs(bounds.topBound.transform.localPosition.y) + Mathf.Abs(bounds.bottomBound.transform.localPosition.y);
+        height /= _navigationManager.tileSize;
+        length = Mathf.Abs(bounds.forwardBound.transform.localPosition.z) + Mathf.Abs(bounds.backBound.transform.localPosition.z);
+        length /= _navigationManager.tileSize;
+
+        roomSize = new Vector3Int(Mathf.RoundToInt(width ), Mathf.RoundToInt(height), Mathf.RoundToInt(length));
+        
+        tiles = new Tile[roomSize.x, roomSize.y,roomSize.z];
+        Debug.Log(tiles.Length);
+    }
+    
+    void GenerateNavigationTilesDebugVisuals()
+    {
+        // 0,0,0 is back left bottom tile
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < length; z++)
+                {
+                    tiles[x, y, z] = new Tile();
+
+                    tiles[x, y, z].worldPosition = transform.position + 
+                            new Vector3(bounds.leftBound.localPosition.x, bounds.bottomBound.localPosition.y, bounds.backBound.position.z) + 
+                        new Vector3( _navigationManager.tileSize * x, _navigationManager.tileSize * y,_navigationManager.tileSize * z);
+                    
+                    Debug.Log(tiles[x, y, z].worldPosition);
+                    var tempPos = tiles[x, y, z].worldPosition;
+                    GameObject tileObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    tileObject.name = "Tile " + x + "; "+ y + "; " + z + ";";
+                    tileObject.transform.position = tempPos;
+                    tileObject.transform.localScale = new Vector3(.5f,.5f,.5f);
+                    tileObject.transform.parent = tilesParent;
+                    
+                    tiles[x,y,z].meshRenderer = tileObject.GetComponent<MeshRenderer>();
+                    tiles[x,y,z].meshRenderer.material = _navigationManager.freeTileMaterial;
+                    
+                    if (tiles[x,y,z].occupied)
+                        tiles[x,y,z].meshRenderer.material = _navigationManager.occupiedTileMaterial;
+
+                    tiles[x, y, z].tileObject = tileObject;
+                    tilesSpawned.Add(tiles[x, y, z]);
+                }
+            }
+        }
+    } 
+    
+    void BoxcastAgainstSpawnedDebugTiles()
+    {
+        int overlappedTilesAmount = 0;
+
+        for (var index = 0; index < tilesSpawned.Count; index++)
+        {
+            var tile = tilesSpawned[index];
+            
+            Collider[] colliders = Physics.OverlapBox(tile.worldPosition, Vector3.one * 2.4f, transform.rotation, obstaclesLayerMask);
+
+            if (colliders.Length > 0)
+            {
+                tile.occupied = true;
+                tile.meshRenderer.material = _navigationManager.occupiedTileMaterial;
+                overlappedTilesAmount++;
+            }
+        }
+
+        Debug.Log("BoxcastAgainstSpawnedDebugTiles; overlappedTilesAmount = " + overlappedTilesAmount);
+    }
+
+    [ContextMenu("ClearSpawnedObject")]
+    public void ClearSpawnedObject()
+    {
+        for (int i = tilesSpawned.Count - 1; i >= 0; i--)
+        {
+            if (tilesSpawned[i].tileObject == null)
+            {
+                continue;
+            }
+
+            DestroyImmediate(tilesSpawned[i].tileObject);
+        }
+    }
+    
+    [ContextMenu("CLEAR TILES")]
+    public void ClearTiles()
+    {
+        for (int i = tilesSpawned.Count - 1; i >= 0; i--)
+        {
+            if (tilesSpawned[i].tileObject == null)
+            {
+                tilesSpawned.RemoveAt(i);
+                continue;
+            }
+
+            DestroyImmediate(tilesSpawned[i].tileObject);
+            tilesSpawned.RemoveAt(i);
+        }
+        tilesSpawned.Clear();
+        tiles = null;
+    }
 }
 
 [Serializable]
 public class Tile
 {
     public GameObject tileObject;
+    public MeshRenderer meshRenderer;
     public Vector3 worldPosition;
     public bool occupied = false;
 }
